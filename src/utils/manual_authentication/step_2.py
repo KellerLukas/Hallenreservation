62595b4745d70cd4c43ba2a7e7b254e6e33e3978
Lukas Keller <keller.lukas@bluewin.ch>
2025-12-22 17:12:20 +0100

    fix bug

--- src/utils/manual_authentication/step_2.py
+++ src/utils/manual_authentication/step_2.py
@@ -1,8 +1,8 @@
-from src.utils.credentials import get_credentials
+from src.utils.credentials import get_o365_credentials_from_env
 from src.utils.fixed_o365_account import FixedAccount
 from src.utils.manual_authentication.step_1 import redirect_uri
 
-credentials = get_credentials()
+credentials = get_o365_credentials_from_env()
 account = FixedAccount(credentials)
 
 token_url = ""  # enter token url here
