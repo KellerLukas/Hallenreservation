e5394dbb7db2ec6b2853a349c3e6bc014bb726a3
Lukas Keller <keller.lukas@bluewin.ch>
2026-01-31 11:02:35 +0100

    add template

--- src/utils/reservation_reminder.py
+++ src/utils/reservation_reminder.py
@@ -11,9 +11,17 @@ from O365.drive import File
 from tempfile import TemporaryDirectory
 from src.utils.find_attachment_meta import get_date_string_from_date
 from src.utils.processor import get_reservations_folder
-from src.utils.config import DEFAULT_FROM_ADDRESS, SUPPORT_EMAIL_ADDRESS
-from src.utils.template.reminder_email_template import template as reminder_email_template
-from src.utils.template.reminder_email_template import reservation_list_template as reminder_email_reservation_list_template
+from src.utils.config import (
+    DEFAULT_FROM_ADDRESS,
+    SUBSCRIPTION_MANAGE_URL,
+    SUPPORT_EMAIL_ADDRESS,
+)
+from src.utils.template.reminder_email_template import (
+    template as reminder_email_template,
+)
+from src.utils.template.reminder_email_template import (
+    reservation_list_template as reminder_email_reservation_list_template,
+)
 
 EMAIL_NEWLINE_STR = "\n<br>\n"
 
@@ -83,7 +91,10 @@ class ReservationReminder:
             f"... found {len(reservations_on_date)} reservations on date {target_day.strftime('%d.%m.%Y')}"
         )
         return self.send_reminder_email(
-            reservations=reservations_on_date, date=target_day, recipients=recipients, n=n
+            reservations=reservations_on_date,
+            date=target_day,
+            recipients=recipients,
+            n=n,
         )
 
     def get_reservations_on_date(self, date: datetime) -> Dict[str, File]:
@@ -98,15 +109,28 @@ class ReservationReminder:
         return matching_files
 
     def send_reminder_email(
-        self, reservations: Dict[str, File], date: datetime, recipients: List[str], n: int
+        self,
+        reservations: Dict[str, File],
+        date: datetime,
+        recipients: List[str],
+        n: int,
     ):
         logging.info("... sending email ...")
         mailbox = self.account.mailbox(resource=DEFAULT_FROM_ADDRESS)
         msg = mailbox.new_message()
 
         msg.subject = f"[TVW Reminder Hallen] Reservation vom {datetime.strftime(date, '%A, %d.%m.%Y')}"
-        reservation_rows = "\n".join(reminder_email_reservation_list_template.format(filename=filename) for filename in reservations.keys())
-        text = reminder_email_template.format(days=n, date=datetime.strftime(date, '%A, %d.%m.%Y'), reservations=reservation_rows)
+        reservation_rows = "\n".join(
+            reminder_email_reservation_list_template.format(filename=filename)
+            for filename in reservations.keys()
+        )
+        text = reminder_email_template.format(
+            days=n,
+            date=datetime.strftime(date, "%A, %d.%m.%Y"),
+            reservations=reservation_rows,
+            subscription_manage_url=SUBSCRIPTION_MANAGE_URL,
+            support_email_address=SUPPORT_EMAIL_ADDRESS,
+        )
 
         msg.body = text
         msg.to.add(DEFAULT_FROM_ADDRESS)
---
+++ src/utils/template/reminder_email_template.py
@@ -0,0 +1,45 @@
+template = """
+<table width="100%" cellpadding="0" cellspacing="0"
+       style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#333333;">
+  <tr>
+    <td>
+
+      <p>Hallo</p>
+
+      <p>
+        In <strong>{days}</strong> Tagen am <strong>{date}</strong> liegen folgende Reservationen vor:
+      </p>
+
+      <table width="100%" cellpadding="0" cellspacing="0" style="margin:10px 0;">
+        {reservations}
+      </table>
+
+      <p>
+        Du erhältst diese Nachricht, weil du den Hallenreservation-Reminder aktiviert hast.
+        Du kannst den Reminder hier anpassen oder abbestellen:
+        <br>
+        {subscription_manage_url}
+      </p>
+
+      <p>
+        Bei Fragen kannst du dich hier melden:
+        {support_email_address}
+      </p>
+
+      <p>
+        Gruss<br>
+        TV Würenlos
+      </p>
+
+    </td>
+  </tr>
+</table>
+"""
+
+reservation_list_template = """
+<tr>
+  <td style="padding:6px 0; border-bottom:1px solid #dddddd;">
+    {filename}
+  </td>
+</tr>
+"""
